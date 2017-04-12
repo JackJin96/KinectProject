7af7c529bcb5b189199ae2af9fb316cccc189b99
Jack <hjin96@bu.edu>
2017-04-12 19:14:49 -0400

    uploading

---
+++ EK132/Assets/Scripts/BalloonsGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonsGenerator : MonoBehaviour {
+
+	public GameObject redBalloon;
+	public GameObject greenBalloon;
+	public float density;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i <= 10; i++) {
+			GameObject balloon;
+			balloon = Random.value > 0.65f ? greenBalloon : redBalloon;
+			Vector3 pos = randomUnitCube((float)i*2 - 10f);
+			Instantiate (balloon, pos, Quaternion.identity);
+		}
+	}
+
+	Vector3 randomUnitCube(float x){
+		return new Vector3 (x, (density*(Random.value) - density/2f), 0f);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ EK132/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+	private Rigidbody rb; 
+	private int count;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	/*void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up")) 
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+	}
+
+	void setCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+	}*/
+}
